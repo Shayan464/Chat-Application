9789af20780d94f790975ecc1b28d18911e488f2
shayan <shayan8839@gmail.com>
2026-04-08 19:30:16 +0530

    message routes optimized

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Message from '../models/Message.js';
 import User from '../models/User.js';
 import { v2 as cloudinary } from 'cloudinary';
@@ -10,8 +11,8 @@ export const getAllContacts = async (req, res) => {
     }).select('-password');
     res.status(200).json(filteredUser);
   } catch (error) {
-    console.loge('error in get all contacts', error);
-    res.status(501).json({ message: 'Server error' });
+    console.error('error in get all contacts', error);
+    res.status(500).json({ message: 'Server error' });
   }
 };
 
@@ -29,7 +30,7 @@ export const getMessagesByUserId = async (req, res) => {
 
     res.status(200).json(messages);
   } catch (error) {
-    console.log('Error in getMessages controller', error.message);
+    console.error('Error in getMessages controller', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
@@ -41,6 +42,14 @@ export const sendMessage = async (req, res) => {
     const senderId = req.user._id;
 
     let imageUrl;
+
+    if (!mongoose.isValidObjectId(receiverId)) {
+      return res.status(400).json({ error: 'Invalid receiver id' });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ error: 'Receiver not found' });
+    }
 
     if (image) {
       //upload base64 image to cloudinary
@@ -61,7 +70,7 @@ export const sendMessage = async (req, res) => {
 
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log('Error in sendMessage controller', error);
+    console.error('Error in sendMessage controller', error);
     res.status(501).json({ error: 'Internal Server error' });
   }
 };
