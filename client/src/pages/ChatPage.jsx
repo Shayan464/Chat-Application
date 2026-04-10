62b939206b40300c61a91e12f142463b86100d42
shayan <shayan8839@gmail.com>
2026-04-10 20:13:54 +0530

    chatPage layout setup

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../features/auth/authSlice';
+import { useSelector } from 'react-redux';
+// import { logout } from '../features/auth/authSlice';
+import ProfileHeader from '../components/ProfileHeader';
+import ActiveTabSwitch from '../components/ActiveTabSwitch';
+import ChatsList from '../components/ChatsList';
+import ContactList from '../components/ContactList';
+import BorderAnimatedContainer from '../components/BorderAnimatedContainer';
+import NoConversationPlaceHolder from '../components/NoConversationPlaceHolder';
+import ChatContainer from '../components/ChatContainer';
 
 const ChatPage = () => {
-  const dispatch = useDispatch();
-  const logoutBtn = () => {
-    dispatch(logout());
-  };
+  // const dispatch = useDispatch();
+
+  const { activeTab, selectedUser } = useSelector((state) => state.chat);
+
   return (
-    <div>
-      <h1>Welcome to the chat page</h1>
-      <button className="btn btn-primary" onClick={logoutBtn}>
-        Click here
-      </button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* left side */}
+        <div className="w-80 bg-slate-800/50 backdrop:blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-auto-y p-4 space-y-2">
+            {activeTab === 'chats' ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceHolder />}
+        </div>
+      </BorderAnimatedContainer>
     </div>
   );
 };
